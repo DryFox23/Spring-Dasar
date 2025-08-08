101278afb178b45846226a50071bfb42edf205e4
bernad <bernadinusnaisau230803@gmail.com>
2025-08-08 22:03:31 +0700

    Belajar Spring Dasar: Membuat application context dengan AnnotationConfigApplicationContext

---
+++ src/test/java/bernadinusnaisau/spring/core/ApplicationContextAppTest.java
@@ -0,0 +1,18 @@
+package bernadinusnaisau.spring.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContextAppTest {
+
+    @Test
+    void testApplicationContext() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+        Assertions.assertNotNull(context);
+
+    }
+}
