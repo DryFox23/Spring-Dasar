afeece20e7f0a5f45fd850e03f1f3b4b04f55b9b
bernad <bernadinusnaisau230803@gmail.com>
2025-08-08 22:18:10 +0700

    Belajar Spring Dasar: Testing databse class dan memastikan object yang dibuat sudah singleton

---
+++ src/test/java/bernadinusnaisau/spring/core/DatabaseTest.java
@@ -0,0 +1,16 @@
+package bernadinusnaisau.spring.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DatabaseTest {
+
+    @Test
+    void testDatabaseClass() {
+
+        Database database1 = Database.getInstance();
+        Database database2 = Database.getInstance();
+
+        Assertions.assertSame(database1, database2);
+    }
+}
