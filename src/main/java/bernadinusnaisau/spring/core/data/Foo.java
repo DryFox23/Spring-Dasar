9a0d862a380c7f8ce43d8287f485b24a2227838b
bernad <bernadinusnaisau230803@gmail.com>
2025-08-13 20:10:12 +0700

    Belajar Spring Dasar: Membuat object bean default dengan annotation @Bean pada object yang dipanggil dan @Configuration pada class utamanya

---
+++ src/main/java/bernadinusnaisau/spring/core/data/Foo.java
@@ -0,0 +1,8 @@
+package bernadinusnaisau.spring.core.data;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+public class Foo {
+}
