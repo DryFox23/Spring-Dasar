1e80059ed53d1cac14bedd75521a40b1ce4457bc
bernad <bernadinusnaisau230803@gmail.com>
2025-08-14 19:44:34 +0700

    Belajar Spring Dasar: Memilih bean mana yang ingin dijalankan menggunakan annotation @Qualifier pada DepedencyInjection classanya

--- src/main/java/bernadinusnaisau/spring/core/DependencyInjectionConfiguration.java
+++ src/main/java/bernadinusnaisau/spring/core/DependencyInjectionConfiguration.java
@@ -3,15 +3,25 @@ package bernadinusnaisau.spring.core;
 import bernadinusnaisau.spring.core.data.Bar;
 import bernadinusnaisau.spring.core.data.Foo;
 import bernadinusnaisau.spring.core.data.FooBar;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+@Slf4j
 @Configuration
 public class DependencyInjectionConfiguration {
 
-    @Bean
-    public Foo foo(){
+    @Primary
+    @Bean(value = "fooFirst")
+    public Foo foo1(){
+        return new Foo();
+    }
+
+    @Bean(value = "fooSecond")
+    public Foo foo2(){
+        log.info("Creating fooSecond");
         return new Foo();
     }
 
@@ -22,7 +32,7 @@ public class DependencyInjectionConfiguration {
 
 
     @Bean
-    public FooBar fooBar(Foo foo, Bar bar){
+    public FooBar fooBar(@Qualifier("fooSecond") Foo foo, Bar bar){
         return new FooBar(foo,bar);
     }
 }
