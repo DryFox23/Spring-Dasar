97576dae91d04c04936e5e89cb8df25f9412f186
bernad <bernadinusnaisau230803@gmail.com>
2025-08-14 20:10:33 +0700

    Belajar Spring Dasar: Membuat Circular Depedencies

---
+++ src/main/java/bernadinusnaisau/spring/core/data/cyclick/CyclickB.java
@@ -0,0 +1,10 @@
+package bernadinusnaisau.spring.core.data.cyclick;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CyclickB {
+    private CyclickC cyclickC;
+}
