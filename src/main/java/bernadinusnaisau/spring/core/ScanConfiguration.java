50a622b31ce22c025bf328452576921f76408157
bernad <bernadinusnaisau230803@gmail.com>
2025-08-15 20:38:48 +0700

    Belajar Spring Dasar: Menyimpan configuration yang berada didalam package dengan Annotation @ComponentScan ini lebih mudah daripada import secara manual

---
+++ src/main/java/bernadinusnaisau/spring/core/ScanConfiguration.java
@@ -0,0 +1,11 @@
+package bernadinusnaisau.spring.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {
+        "bernadinusnaisau.spring.core.configuration"
+})
+public class ScanConfiguration {
+}
