dd0a406374f1acb660579d04ab105068f90c70a0
bernad <bernadinusnaisau230803@gmail.com>
2025-08-15 19:49:44 +0700

    Belajar Spring Dasar: Integrasi lifecycle dengan annotation @Bean dan menambahkan initMethod sebagai tanda mulainya dan destroyMethod sebagai tanda stopnya

--- src/main/java/bernadinusnaisau/spring/core/LifecycleConfiguration.java
+++ src/main/java/bernadinusnaisau/spring/core/LifecycleConfiguration.java
@@ -1,6 +1,7 @@
 package bernadinusnaisau.spring.core;
 
 import bernadinusnaisau.spring.core.data.Connection;
+import bernadinusnaisau.spring.core.data.cyclick.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +11,10 @@ public class LifecycleConfiguration {
     @Bean
     public Connection connection(){
         return new Connection();
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server server(){
+        return new Server();
     }
 }
