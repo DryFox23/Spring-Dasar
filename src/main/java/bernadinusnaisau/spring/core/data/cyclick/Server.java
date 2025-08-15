4e73b982ce8b74cb119860f969e7d7889586ac51
bernad <bernadinusnaisau230803@gmail.com>
2025-08-15 19:53:19 +0700

    Belajar Spring Dasar: Integrasi lifecycle dengan cara metode menambahkan Annotation pada class yang ingin dijadikan beanya @PostConstruct sebagai tanda dimulainya lifecycle dan @PreDestroy sebagai tanda stopnya lifecycle

--- src/main/java/bernadinusnaisau/spring/core/data/cyclick/Server.java
+++ src/main/java/bernadinusnaisau/spring/core/data/cyclick/Server.java
@@ -1,14 +1,18 @@
 package bernadinusnaisau.spring.core.data.cyclick;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Server {
 
+    @PostConstruct
     public void start(){
         log.info("Server Started");
     }
 
+    @PreDestroy
     public void stop(){
         log.info("Server Stopped");
     }
