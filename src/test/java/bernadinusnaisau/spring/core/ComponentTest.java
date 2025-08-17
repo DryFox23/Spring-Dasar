4f96ff142f8b31793e2a4d5ffcc06cb898f50081
bernad <bernadinusnaisau230803@gmail.com>
2025-08-17 19:24:17 +0700

    Belajar Spring Dasar: Testing annotation @Component dan @ComponentScan didalam sebuah bean object

---
+++ src/test/java/bernadinusnaisau/spring/core/ComponentTest.java
@@ -0,0 +1,33 @@
+package bernadinusnaisau.spring.core;
+
+import bernadinusnaisau.spring.core.data.Foo;
+import bernadinusnaisau.spring.core.service.ProductService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ComponentTest {
+
+    private ConfigurableApplicationContext context;
+
+    @BeforeEach
+    void setUp() {
+        context = new AnnotationConfigApplicationContext(ComponentConfiguration.class);
+        context.registerShutdownHook();
+    }
+
+    @Test
+    void testComponent() {
+        ProductService productService1 = context.getBean(ProductService.class);
+        ProductService productService2 = context.getBean("productService",ProductService.class);
+
+        Assertions.assertSame(productService1, productService2);
+    }
+
+    @Test
+    void testFoo() {
+        Foo foo = context.getBean(Foo.class);
+    }
+}
