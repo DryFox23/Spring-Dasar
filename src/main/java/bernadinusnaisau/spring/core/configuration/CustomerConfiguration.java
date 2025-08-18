18f5a59b6494ab005b671b00b8b0779b32292e51
bernad <bernadinusnaisau230803@gmail.com>
2025-08-18 19:47:55 +0700

    Belajar Spring Dasar: pembuatan 2 bean object yang sama

---
+++ src/main/java/bernadinusnaisau/spring/core/configuration/CustomerConfiguration.java
@@ -0,0 +1,20 @@
+package bernadinusnaisau.spring.core.configuration;
+
+import bernadinusnaisau.spring.core.repository.CustomerRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class CustomerConfiguration {
+
+    @Bean
+    public CustomerRepository normalCustomerRepository() {
+        return new CustomerRepository();
+    }
+
+    @Bean
+    public CustomerRepository premiumCustomerRepository() {
+        return new CustomerRepository();
+    }
+}
