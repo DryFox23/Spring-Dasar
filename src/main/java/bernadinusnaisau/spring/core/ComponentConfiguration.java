0b38af6f23756171b14bc7b98e73af80b7895ed2
bernad <bernadinusnaisau230803@gmail.com>
2025-08-18 20:41:24 +0700

    Belajar Spring Dasar: Import class yang dibutuhkan untuk ObjectProvider

--- src/main/java/bernadinusnaisau/spring/core/ComponentConfiguration.java
+++ src/main/java/bernadinusnaisau/spring/core/ComponentConfiguration.java
@@ -1,13 +1,18 @@
 package bernadinusnaisau.spring.core;
 
+import bernadinusnaisau.spring.core.data.MultiFoo;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = {
         "bernadinusnaisau.spring.core.repository",
         "bernadinusnaisau.spring.core.service",
         "bernadinusnaisau.spring.core.configuration"
+})
+@Import({
+        MultiFoo.class
 })
 public class ComponentConfiguration {
 }
