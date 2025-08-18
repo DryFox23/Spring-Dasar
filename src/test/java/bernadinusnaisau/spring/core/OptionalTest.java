2bf00ee5136fd5901a9bbe5215b022a398712333
bernad <bernadinusnaisau230803@gmail.com>
2025-08-18 20:43:13 +0700

    Belajar Spring Dasar: Testing memanggil dependecy injection menggunakan Optional<T>

---
+++ src/test/java/bernadinusnaisau/spring/core/OptionalTest.java
@@ -0,0 +1,31 @@
+package bernadinusnaisau.spring.core;
+
+import bernadinusnaisau.spring.core.configuration.OptionalConfiguration;
+import bernadinusnaisau.spring.core.data.Foo;
+import bernadinusnaisau.spring.core.data.FooBar;
+import bernadinusnaisau.spring.core.data.MultiFoo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class OptionalTest {
+
+    private ConfigurableApplicationContext context;
+
+    @BeforeEach
+    void setUp() {
+        context = new AnnotationConfigApplicationContext(OptionalConfiguration.class);
+    }
+
+    @Test
+    void testOptional() {
+        Foo foo = context.getBean(Foo.class);
+        FooBar fooBar = context.getBean(FooBar.class);
+
+        Assertions.assertNull(fooBar.getBar());
+        Assertions.assertSame(foo, fooBar.getFoo());
+    }
+
+}
