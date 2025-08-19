fb35bacf23fafde5d3e3103beac16390e955c711
bernad <bernadinusnaisau230803@gmail.com>
2025-08-19 19:36:55 +0700

    Belajar Spring Dasar: Import class PrefixIdGeneratorBeanProcessor sebagai beanPostProcessor yang ke 2

--- src/main/java/bernadinusnaisau/spring/core/BeanPostProcessorConfiguration.java
+++ src/main/java/bernadinusnaisau/spring/core/BeanPostProcessorConfiguration.java
@@ -3,13 +3,15 @@ package bernadinusnaisau.spring.core;
 import bernadinusnaisau.spring.core.aware.IdAware;
 import bernadinusnaisau.spring.core.data.Car;
 import bernadinusnaisau.spring.core.processor.IdGeneratorBeansPostProcessor;
+import bernadinusnaisau.spring.core.processor.PrefixIdGeneratorBeansProcessor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
         Car.class,
-        IdGeneratorBeansPostProcessor.class
+        IdGeneratorBeansPostProcessor.class,
+        PrefixIdGeneratorBeansProcessor.class
 })
 public class BeanPostProcessorConfiguration {
 }
