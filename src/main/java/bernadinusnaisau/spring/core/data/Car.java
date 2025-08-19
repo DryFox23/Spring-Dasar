5205557a7c592723070f102091f1038fa7196954
bernad <bernadinusnaisau230803@gmail.com>
2025-08-19 19:07:41 +0700

    Belajar Spring Dasar: Implementasi beanPostProcessor sebagai penanda sebelum / sesudah initializing bean

---
+++ src/main/java/bernadinusnaisau/spring/core/data/Car.java
@@ -0,0 +1,17 @@
+package bernadinusnaisau.spring.core.data;
+
+import bernadinusnaisau.spring.core.aware.IdAware;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car implements IdAware {
+
+    @Getter
+    private String id;
+
+    @Override
+    public void setId(String id) {
+    this.id = id;
+    }
+}
