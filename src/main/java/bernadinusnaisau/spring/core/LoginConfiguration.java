a206d7b0a29885e3b83f224d3aa5571542a5695b
bernad <bernadinusnaisau230803@gmail.com>
2025-08-20 20:47:06 +0700

    Belajar Spring Dasar: Penambahan import class untuk event listener

--- src/main/java/bernadinusnaisau/spring/core/LoginConfiguration.java
+++ src/main/java/bernadinusnaisau/spring/core/LoginConfiguration.java
@@ -1,6 +1,7 @@
 package bernadinusnaisau.spring.core;
 
 import bernadinusnaisau.spring.core.listener.LoginSuccesListener;
+import bernadinusnaisau.spring.core.listener.UserListener;
 import bernadinusnaisau.spring.core.service.UserService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -8,7 +9,8 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({
         LoginSuccesListener.class,
-        UserService.class
+        UserService.class,
+        UserListener.class
 })
 public class LoginConfiguration {
 }
