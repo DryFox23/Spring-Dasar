63c3eb4220b69f79157db7b98c407ce7da50771e
bernad <bernadinusnaisau230803@gmail.com>
2025-08-21 19:58:58 +0700

    Belajar Spring Dasar: Implementasi interface ApplicationRunner

---
+++ src/main/java/bernadinusnaisau/spring/core/runnerapp/RunnerApplication.java
@@ -0,0 +1,11 @@
+package bernadinusnaisau.spring.core.runnerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunnerApplication.class);
+    }
+}
---
+++ src/main/java/bernadinusnaisau/spring/core/runnerapp/SimpleRunner.java
@@ -0,0 +1,19 @@
+package bernadinusnaisau.spring.core.runnerapp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+public class SimpleRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        List<String> profile = args.getOptionValues("profile");
+        log.info("Running Simple Runner : {}", profile);
+    }
+}
