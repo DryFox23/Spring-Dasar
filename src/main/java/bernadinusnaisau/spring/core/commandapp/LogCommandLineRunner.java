1068431de0cb08f2da3ec115154332ff8fa0dddc
bernad <bernadinusnaisau230803@gmail.com>
2025-08-21 18:09:45 +0700

    Belajar Spring Dasar: Implementasi interface CommandLineRunner

---
+++ src/main/java/bernadinusnaisau/spring/core/commandapp/LogCommandLineRunner.java
@@ -0,0 +1,15 @@
+package bernadinusnaisau.spring.core.commandapp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LogCommandLineRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+    log.info("Starting Command Line Runner : {}", args);
+    }
+}
