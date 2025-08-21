ad6faee9599c084b90f191077e517cdad498a525
bernad <bernadinusnaisau230803@gmail.com>
2025-08-21 17:41:44 +0700

    Belajar Spring Dasar: Kostumisasi penggunaan banner agar tidak muncul (bisa banyak mengkonfigurasi apapun didalamnya)

--- src/main/java/bernadinusnaisau/spring/core/application/FooApplication.java
+++ src/main/java/bernadinusnaisau/spring/core/application/FooApplication.java
@@ -2,6 +2,7 @@ package bernadinusnaisau.spring.core.application;
 
 import bernadinusnaisau.spring.core.data.Bar;
 import bernadinusnaisau.spring.core.data.Foo;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,13 +12,24 @@ import org.springframework.context.annotation.Bean;
 public class FooApplication {
 
     @Bean(value = "Foo Bean")
-    public Foo foo(Bar bar) {
+    public Foo foo() {
         return new Foo();
     }
 
+//    public static void main(String[] args) {
+//        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+//        Foo foo = applicationContext.getBean("Foo Bean", Foo.class);
+//        System.out.println(foo);
+//    }
+
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
-        Foo foo = applicationContext.getBean("Foo Bean", Foo.class);
+        SpringApplication app = new SpringApplication(FooApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+
+        ConfigurableApplicationContext context = app.run(args);
+        Foo foo = context.getBean(Foo.class);
         System.out.println(foo);
     }
+
+
 }
