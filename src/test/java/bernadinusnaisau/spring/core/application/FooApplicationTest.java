1047458fc42a8c1bcbd9fc88ddb81db4b86de0e7
bernad <bernadinusnaisau230803@gmail.com>
2025-08-21 17:22:25 +0700

    Belajar Spring Dasar: Membandingkan error console dengan menggunakan spring boot

--- src/test/java/bernadinusnaisau/spring/core/application/FooApplicationTest.java
+++ src/test/java/bernadinusnaisau/spring/core/application/FooApplicationTest.java
@@ -1,15 +1,11 @@
-package bernadinusnaisau.spring.core;
+package bernadinusnaisau.spring.core.application;
 
-import bernadinusnaisau.spring.core.application.BarApplication;
-import bernadinusnaisau.spring.core.application.FooApplication;
-import bernadinusnaisau.spring.core.configuration.BarConfiguration;
 import bernadinusnaisau.spring.core.data.Bar;
 import bernadinusnaisau.spring.core.data.Foo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootTest(classes = {FooApplication.class, BarApplication.class})
 public class FooApplicationTest {
